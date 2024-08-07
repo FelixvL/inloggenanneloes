774f988e183943f5a4d319a1fdb8f5ff142a0b67
felixvl <digitaleservice@gmail.com>
2024-08-07 14:34:43 +0200

    inloggen controleert op wachtwoord en password

--- index.php
+++ index.php
@@ -1,15 +1,24 @@
+<?php
+    if(isset($_GET["geenGebruikersnaam"])){
+        echo "foute username";
+    }
+    if(isset($_GET["foutWachtwoord"])){
+        echo "foute wachtwoord";
+    }
+?>
+
 <script>
     function inlogGegevensControleren(){
         let gebruikersnaam = document.getElementById("gebruikersnaamVeld").value;
         let wachtwoord = document.getElementById("wachtwoordVeld").value;
 
-        window.open("inloggegevenscontroleren.php?gebruikersnaampv="+gebruikersnaam, "_self");
+        window.open("inloggegevenscontroleren.php?gebruikersnaampv="+gebruikersnaam+"&wachtwoordpv="+wachtwoord, "_self");
     }
 </script>
 
 <h1>inloggen</h1>
 
 Username: <input id="gebruikersnaamVeld" ><br>
-Wachtwoord: <input id="wachtwoordVeld" ><br>
+Wachtwoord: <input type="password" id="wachtwoordVeld" ><br>
 
 <button onclick="inlogGegevensControleren()">Inloggen</button>
--- inloggegevenscontroleren.php
+++ inloggegevenscontroleren.php
@@ -1,11 +1,22 @@
 <?php
 $gebruikersnaam = $_GET["gebruikersnaampv"];
+$wachtwoord = $_GET["wachtwoordpv"];
 
 $mysqli = new mysqli("localhost","root","","anneloesonzedatabase");
 
 $sql = "SELECT * FROM schrijver WHERE username = '".$gebruikersnaam."';";
-$resultaat = $mysqli->query($sql);
-$rij = $resultaat -> fetch_assoc();
+$resultaat = $mysqli -> query($sql);
 
-echo "Welkom: ".$rij["voornaam"];
+if($resultaat -> num_rows != 1){
+    header("Location: index.php?geenGebruikersnaam=nietgevonden");
+}else{
+    $rij = $resultaat -> fetch_assoc();
+    if($rij["wachtwoord"] == $wachtwoord){
+        echo "Welkom: ".$rij["voornaam"];
+        echo "<br>";
+        echo "ga hier nu aan de slag met blogs van ".$rij["id"];
+    }else{
+        header("Location: index.php?foutWachtwoord=incorrect"); 
+    }
+}
 ?>
