9b28cde34ade6caeeb1e758cba83366d0a1ba870
felixvl <digitaleservice@gmail.com>
2024-08-07 13:51:45 +0200

    inloggegevens controleren

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
+<script>
+    function inlogGegevensControleren(){
+        let gebruikersnaam = document.getElementById("gebruikersnaamVeld").value;
+        let wachtwoord = document.getElementById("wachtwoordVeld").value;
+        console.log(gebruikersnaam, wachtwoord);
+    }
+</script>
+
 <h1>inloggen</h1>
 
-Username: <input id="inlognaam" ><br>
-Wachtwoord: <input id="wachtwoord" ><br>
-<button>Inloggen</button>
+Username: <input id="gebruikersnaamVeld" ><br>
+Wachtwoord: <input id="wachtwoordVeld" ><br>
+
+<button onclick="inlogGegevensControleren()">Inloggen</button>
